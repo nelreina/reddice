0ec73b596d81d7d6562a8200e999443c9fb0f654
Nelreina <nelreina@gmail.com>
2016-08-06 12:28:18 -0400

    Render main react component

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMW from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev.js';
+
 const app = express();
 
+app.use(webpackMW(webpack(webpackConfig)));
+
+
 app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 /* eslint-disable */
 app.listen(3000, () => console.log('App listen to localhost:3000'))
